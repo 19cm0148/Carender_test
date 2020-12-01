c82c45c712f34456aaddb0663cd85a800ffac428
Mita <19cm0148@jec.ac.jp>
2020-12-01 11:55:28 +0900

    test

---
+++ Carender_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carender_test
+//
+//  Created by Soma Mita on 2020/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("test")
+    }
+
+}
+
